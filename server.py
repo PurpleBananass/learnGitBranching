af020fe7719a7b462d23fcf5937ca7a254140818
PurpleBananass <joony@ijun-uui-Macmini.local>
2023-09-18 14:25:56 +0900

    Server Fix python3

--- server.py
+++ server.py
@@ -1,44 +1,35 @@
-import time, threading, socket, SocketServer, BaseHTTPServer
+#!/usr/bin/env python3
+import sys, os, socket
+from socketserver import ThreadingMixIn
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 
-class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
+HOST = socket.gethostname()
 
-    def do_GET(self):
-        if self.path != '/':
-            self.send_error(404, "Object not found")
-            return
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html; charset=utf-8')
-        self.end_headers()
+class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
+    pass
 
-        # serve up an infinite stream
-        i = 0
-        while True:
-            self.wfile.write("%i " % i)
-            time.sleep(0.1)
-            i += 1
+'''
+This sets the listening port, default port 8080
+'''
+if sys.argv[1:]:
+    PORT = int(sys.argv[1])
+else:
+    PORT = 8080
 
-# Create ONE socket.
-addr = ('', 8080)
-sock = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind(addr)
-sock.listen(5)
+'''
+This sets the working directory of the HTTPServer, defaults to directory where script is executed.
+'''
+if sys.argv[2:]:
+    os.chdir(sys.argv[2])
+    CWD = sys.argv[2]
+else:
+    CWD = os.getcwd()
 
-# Launch 100 listener threads.
-class Thread(threading.Thread):
-    def __init__(self, i):
-        threading.Thread.__init__(self)
-        self.i = i
-        self.daemon = True
-        self.start()
-    def run(self):
-        httpd = BaseHTTPServer.HTTPServer(addr, Handler, False)
-
-        # Prevent the HTTP server from re-binding every handler.
-        # https://stackoverflow.com/questions/46210672/
-        httpd.socket = sock
-        httpd.server_bind = self.server_close = lambda self: None
-
-        httpd.serve_forever()
-[Thread(i) for i in range(100)]
-time.sleep(9e9)
+server = ThreadingSimpleServer(('0.0.0.0', PORT), SimpleHTTPRequestHandler)
+print("Serving HTTP traffic from", CWD, "on", HOST, "using port", PORT)
+try:
+    while 1:
+        sys.stdout.flush()
+        server.handle_request()
+except KeyboardInterrupt:
+    print("\nShutting down server per users request.")
